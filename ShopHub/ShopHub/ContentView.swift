076fe0bfb166fa7bc758077bba744fd1d2f6e3d3
Yongye Tan <yongyetan1209@gmail.com>
2023-08-11 21:59:06 -0400

    Initial commit

---
+++ ShopHub/ShopHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShopHub
+//
+//  Created by Yongye Tan on 8/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShopHub/ShopHub/ShopHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopHubApp.swift
+//  ShopHub
+//
+//  Created by Yongye Tan on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ShopHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
