06fcb714e3dba6eb22e1744ff96a11cbf3763a34
Yongye Tan <yongyetan1209@gmail.com>
2023-08-12 01:58:33 -0400

    Add MenuTab view Model

--- ShopHub/ShopHub/ContentView.swift
+++ ShopHub/ShopHub/ContentView.swift
@@ -11,18 +11,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             TabView {
-                ProductsView()
-                    .tabItem {
-                        Label("Product", systemImage: "bag")
-                    }
-                CartView()
-                    .tabItem {
-                        Label("Cart", systemImage: "cart")
-                    }
-                UserView()
-                    .tabItem {
-                        Label("User", systemImage: "person")
-                    }
+                MenuTabView()
             }
             .tabViewStyle(DefaultTabViewStyle())
             .navigationTitle("ShopHub")
---
+++ ShopHub/ShopHub/User Interface/Menu Navigation/MenuTabView.swift
@@ -0,0 +1,62 @@
+//
+//  MenuTabView.swift
+//  ShopHub
+//
+//  Created by Yongye Tan on 8/12/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct MenuTabView: View {
+    @StateObject var menuViewModel: MenuTabViewModel = MenuTabViewModel()
+    
+    var body: some View {
+        ForEach(menuViewModel.tabs) { tab in
+            tab.destination
+                .tabItem {
+                    tab.label
+                }
+        }
+    }
+}
+
+/// a view model that stores the information about the menu tabs
+final class MenuTabViewModel: ObservableObject {
+    @Published var tabs: [MenuTab] = MenuTab.allCases
+}
+
+/// A enum that represents the menu tab at the bottom of the screen
+enum MenuTab: CaseIterable, Identifiable, Codable, Hashable {
+    case products
+    case cart
+    case user
+    
+    var id: MenuTab { return self }
+}
+
+extension MenuTab {
+    @ViewBuilder
+    var label: some View {
+        switch self {
+        case .products:
+            Label("Product", systemImage: "bag")
+        case .cart:
+            Label("Cart", systemImage: "cart")
+        case .user:
+            Label("User", systemImage: "person")
+        }
+    }
+    
+    @ViewBuilder
+    var destination: some View {
+        switch self {
+        case .products:
+            ProductsView()
+        case .cart:
+            CartView()
+        case .user:
+            UserView()
+        }
+    }
+}
