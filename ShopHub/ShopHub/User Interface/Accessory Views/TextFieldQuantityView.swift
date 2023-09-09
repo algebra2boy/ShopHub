17fa73c65b70ce9192148f1e5f534e098650550d
Yongye Tan <yongyetan1209@gmail.com>
2023-09-09 13:47:23 -0400

    Add comments to textfield quantity view

--- ShopHub/ShopHub/User Interface/Accessory Views/TextFieldQuantityView.swift
+++ ShopHub/ShopHub/User Interface/Accessory Views/TextFieldQuantityView.swift
@@ -13,16 +13,17 @@ struct TextFieldQuantityView: View {
     @FocusState var focusState: Bool
     
     init(value: Binding<Int>, focusState: FocusState<Bool>) {
-        _value = value
-        _focusState = focusState
+        // since we pass value and focusState as binding variable, we need to get the wrapped value of these binding variable, therefore having the underscore "opens" the Binding for you to change its wrapped value. More to find: https://stackoverflow.com/a/71237371/22303588
+        self._value = value
+        self._focusState = focusState
     }
     
     var body: some View {
         TextField("1", value: $value, format: .number)
+            .frame(width: 55)
             .focused($focusState)
             .multilineTextAlignment(.center)
             .textFieldStyle(.roundedBorder)
-            .frame(width: 55)
             .keyboardType(.numberPad) // super weird log: unable to simultaneously satisfy constraints, seems like a bug in swiftui, more to find: https://stackoverflow.com/questions/66082340/swiftui-unable-to-simultaneously-satisfy-constraints-when-focus-on-textfield-a
             .onChange(of: value) { oldValue, newValue in
                 if value > 999 {
