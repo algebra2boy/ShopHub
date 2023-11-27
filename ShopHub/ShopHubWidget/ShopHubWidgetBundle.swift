6f0017761ca6a9b9ec50df94fab4e6c33e56b313
Chengtao <chengtaolinctl@gmail.com>
2023-11-27 15:20:16 -0500

    initialize the widget extension

---
+++ ShopHub/ShopHubWidget/ShopHubWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  ShopHubWidgetBundle.swift
+//  ShopHubWidget
+//
+//  Created by CHENGTAO on 11/27/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ShopHubWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ShopHubWidget()
+        ShopHubWidgetLiveActivity()
+    }
+}
