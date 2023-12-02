3f401c1fc4d7d373791a14fc02568b0da198dd11
Yongye Tan <yongyetan1209@gmail.com>
2023-12-02 01:25:29 -0500

    Organize code

--- ShopHub/ShopHub/User Interface/Page Views/Products/Sale/SalesView.swift
+++ ShopHub/ShopHub/User Interface/Page Views/Products/Sale/SalesView.swift
@@ -14,28 +14,42 @@ struct SalesView: View {
     
     @Environment(ShopHubViewModel.self) private var viewModel
     
+    // Parameter
+    
     let columns = [GridItem](repeating: GridItem(.flexible()), count: 2)
     
     
     var body: some View {
         ScrollView {
+            
             HStack {
+                
                 Text("Sales")
                     .font(.system(size: 45))
                     .padding(.horizontal, 20)
                     .fontWeight(.bold)
+                
                 Spacer()
+                
             }
             .padding(-2)
+            
             LazyVGrid(columns: columns, spacing: 20) {
+                
                 ForEach(viewModel.allProducts) { product in
+                    
                     NavigationLink {
+                        
                         ProductDetailView(product: product)
+                        
                     } label: {
+                        
                         ProductView(product: product)
                     }
+                    
                 }
             }
+            
         }
     }
 }
